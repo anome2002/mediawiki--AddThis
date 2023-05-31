777564894a56dd62bc4532de34004873bf57d52a
anome2002 <126911875+anome2002@users.noreply.github.com>
2023-05-31 14:05:20 +0300

    Update AddThis.php

--- AddThis.php
+++ AddThis.php
@@ -40,7 +40,7 @@ $wgOut->addHTML('
     <script>
         function generateSocialSharingWidget() {
             var url = window.location.href;
-            var socialSharingHTML = \'<div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="\' + url + \'" data-a2a-title="Check out this awesome website!">\'
+            var socialSharingHTML = \'<div class="a2a_kit a2a_kit_size_21 a2a_default_style" data-a2a-url="\' + url + \'" data-a2a-title="Check out this awesome website!">\'
                 + \'<a class="a2a_button_facebook"></a>\'
                 + \'<a class="a2a_button_twitter"></a>\'
                 + \'<a class="a2a_button_linkedin"></a>\'
